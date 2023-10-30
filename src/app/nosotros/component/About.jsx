b468c204ac7925d5d6ac7724907fcdee8afaf57b
sibelius303 <74629371+sibelius303@users.noreply.github.com>
2023-10-30 13:42:20 -0300

    correciones en el fondo

--- src/app/nosotros/component/About.jsx
+++ src/app/nosotros/component/About.jsx
@@ -16,12 +16,12 @@ export default function About() {
 	}, []);
 
 	return (
-		<div  className=" h-auto lg:px-20 pt-10 pb-20 rounded-md bg-slate-100">
+		<div  className=" h-auto lg:px-20 pt-10 pb-20 rounded-md">
 			<div  className=" flex flex-col lg:flex-row lg:justify-between items-center gap-4 lg:gap-2 mt-14">
-				<div className="shadow-lg bg-white w-full rounded-md  ">
+				<div className="shadow-lg  w-full rounded-md  ">
 					<Image width={700} height={500} className=" rounded-md" src={"/sobreLaEmpresa.jpeg"} alt="Imagen de muestra" />
 				</div>
-				<div className="lg:absolute lg:right-20 lg:w-1/2 bg-gradient-to-r from-azulPantone to-rojoPantone text-white  lg:max-h-72  p-4 rounded-md">
+				<div className="lg:absolute lg:right-20 lg:w-1/2 bg-rojoPantone text-white  lg:max-h-72  p-4 rounded-md">
 					<h1 className="text-4xl font-bold mb-8 text-center">Sobre Nosotros</h1>
 					<p className="text-lg">Corpoelec Industrial fue creada el 13 de agosto de 2013, a través del Decreto Presidencial
 						N° 321, publicado en la Gaceta Oficial N° 40.227, cuando el Presidente de la República,
@@ -32,10 +32,10 @@ export default function About() {
 				</div>
 			</div>
 			<div data-aos="fade-up" className="flex flex-col lg:flex-row lg:justify-between items-center gap-4 lg:gap-2 mt-14">
-				<div className=" w-full flex lg:hidden justify-end h-auto overflow-hidden shadow-lg bg-white rounded-md">
+				<div className=" w-full flex lg:hidden justify-end h-auto overflow-hidden shadow-lg  rounded-md">
 					<Image width={700} height={500} className="rounded-md" src={"/mision.jpeg"} alt="Imagen de muestra" />
 				</div>
-				<div className="lg:absolute lg:w-1/2 bg-gradient-to-r from-azulPantone to-rojoPantone lg:max-h-56 text-white p-4 rounded-md">
+				<div className="lg:absolute lg:w-1/2 bg-rojoPantone lg:max-h-56 text-white p-4 rounded-md">
 					<h1 className="text-4xl font-bold mb-4 text-center">Misión</h1>
 					<p className="text-lg">“Fortalecer un nuevo modelo productivo
 						socialista a través de la producción de bienes y
@@ -43,16 +43,16 @@ export default function About() {
 						que contribuya e impulsen el desarrollo del Sector
 						Eléctrico Nacional (SEN)”</p>
 				</div>
-				<div className="hidden lg:flex w-full  justify-end h-auto overflow-hidden shadow-lg bg-white rounded-md">
+				<div className="hidden lg:flex w-full  justify-end h-auto overflow-hidden shadow-lg  rounded-md">
 					<Image width={700} height={500} className="rounded-md" src={"/mision.jpeg"} alt="Imagen de muestra" />
 				</div>
 			</div>
 
 			<div data-aos="fade-up" className=" flex flex-col lg:flex-row lg:justify-between items-center gap-4 lg:gap-2 mt-14">
-				<div className="overflow-hidden shadow-lg bg-white w-full rounded-md">
+				<div className="overflow-hidden shadow-lg  w-full rounded-md">
 					<Image width={700} height={500} className="rounded-md" src="/vision.JPG" alt="Imagen de muestra" />
 				</div>
-				<div className="lg:absolute lg:right-0 lg:w-1/2 bg-gradient-to-r from-azulPantone to-rojoPantone text-white  lg:max-h-64  p-4 rounded-md">
+				<div className="lg:absolute lg:right-0 lg:w-1/2 bg-rojoPantone text-white  lg:max-h-64  p-4 rounded-md">
 					<h1 className="text-4xl font-bold mb-4 text-center  ">Visión</h1>
 					<p className="text-lg">Posicionarnos en el mercado internacional
 						como modelo de gestión pública, nuestra
@@ -69,7 +69,7 @@ export default function About() {
 				<div>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nihil temporibus delectus a, sapiente omnis, earum unde ducimus optio explicabo molestias ipsum blanditiis! Rerum expedita eligendi illo, quaerat eius molestiae.</p>
 				</div>
-				<div className="flex gap-4 flex-col lg:flex-row rounded overflow-hidden shadow-lg bg-white">
+				<div className="flex gap-4 flex-col lg:flex-row rounded overflow-hidden shadow-lg ">
 					<div>
 						<img className="w-full" src="https://via.placeholder.com/600x300" alt="Imagen de muestra" />
 						<div className="px-6 py-4">
