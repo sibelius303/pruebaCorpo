a2a5041ef342e4da2b162bcae16505c1ec1b71fb
Octavio Martinez <omartinez1618@gmail.com>
2023-11-27 16:19:00 -0400

    fix

--- src/components/frontpage/FrontPage.jsx
+++ src/components/frontpage/FrontPage.jsx
@@ -39,6 +39,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'
 import { useEffect } from 'react';
 import Contactenos from '../Contactenos'
+import NewsSlider from '../NewsSlider'
 
 const currencies = ['BS', 'USD']
 const navigation = {
@@ -316,6 +317,9 @@ export default function FrontPage() {
 					<Image src={'/vietverdetransparente.png'} width={200} height={200} alt='imagen no disponible' />
 					<Image src={'/logosinfondo.png'} width={250} height={250} alt='imagen no disponible' />
 				</div>
+				{/* <div className='h-[300px] w-full flex pl-4 border border-red-500'> */}
+					<NewsSlider />
+				{/* </div> */}
 				<div className='h-96 w-full flex pl-4'>
 					<div className='w-1/2 flex flex-col items-center justify-center gap-y-5'>
 						<h1 className="text-xl font-bold">Pague su medidor</h1>
