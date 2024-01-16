7ec8ef117279bbc7fd58d0e04a63762291276171
sibelius303 <74629371+sibelius303@users.noreply.github.com>
2024-01-15 22:55:02 -0300

    correccion en la imagen

--- src/app/layout.js
+++ src/app/layout.js
@@ -67,7 +67,7 @@ export default function RootLayout({ children }) {
 						estiloCintilloGris
 					} className="flex w-full items-center justify-between">
 						<div className='w-full py-5 hidden lg:flex items-center justify-around'>
-							<Image src={'/MPPEE.png'} width={200} height={50} alt='imagen no disponible' />
+							<Image src={'/mppee.png'} width={200} height={50} alt='imagen no disponible' />
 							<Image src={'/logosinfondo.png'} width={250} height={250} alt='imagen no disponible' />
 							<Image src={'/corpoImagenes/unervensinfondorecortada.png'} width={200} height={50} alt='imagen no disponible' />
 							<Image src={'/corpoImagenes/vietvensinfondorecortada.png'} width={200} height={50} alt='imagen no disponible' />
@@ -77,7 +77,7 @@ export default function RootLayout({ children }) {
 
 				<div className='h-48 grid grid-cols-2 lg:hidden items-center justify-around'>
 					<div className='flex items-center justify-center'>
-						<Image src={'/MPPEE.png'} width={100} height={100} alt='imagen no disponible' />
+						<Image src={'/mppee.png'} width={100} height={100} alt='imagen no disponible' />
 					</div>
 					<div className='flex items-center justify-center'>
 						<Image src={'/logosinfondo.png'} width={125} height={125} alt='imagen no disponible' />
